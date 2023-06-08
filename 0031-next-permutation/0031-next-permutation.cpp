ecaaab547ec820f4c63ea50c08db51d8b37d83be
Suman Moond <sumanmoond2021@gmail.com>
2023-06-08 12:15:02 +0530

    Time: 5 ms (41.45%), Space: 12 MB (63.26%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        // int index=0;
+        // do{
+        //     if(index==1) break;
+        //     index++;
+        // }
+        // while(next_permutation(nums.begin(),nums.end()));
+        next_permutation(nums.begin(),nums.end());
+
+    }
+};
