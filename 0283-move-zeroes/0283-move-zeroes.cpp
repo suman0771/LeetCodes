0504aa7ea6afa6474813577c91ad568f1c18158b
Suman Moond <sumanmoond2021@gmail.com>
2023-08-08 12:32:24 +0530

    Time: 12 ms (98.06%), Space: 19.3 MB (11.22%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==0){
+        //         count++;
+        //     }
+        // }
+        int keep=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[keep]);
+                keep++;
+            }
+        }
+    }
+};
